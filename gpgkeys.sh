199404675848575df4b433b406761384859a8807
brokenpip3 <brokenpip3@gmail.com>
2020-10-22 01:11:39 +0200

    fixup

--- gpgkeys.sh
+++ gpgkeys.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-printf "-- Removing comment -- \n"
+printf '-- Removing comment -- \n'
 sed -i -e "/\s*#.*/s/\s*#.*//" -e "/^\s*$/d" gpgkeys
 
-printf "-- Retriving gpgkeys -- \n"
+printf '-- Retriving gpgkeys -- \n'
 while read line; do 
 		gpg --recv-keys --keyserver 'hkp://ipv4.pool.sks-keyservers.net' $line
 done < gpgkeys
