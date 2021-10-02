05c744d5d29d84694ec6a219c00ef35f6b4f8a83
brokenpip3 <brokenpip3@gmail.com>
2021-10-02 14:35:00 +0200

    adjust sh and path while we have different jenkinsfile

--- build-package/gpgkeys.sh
+++ build-package/gpgkeys.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+rundir=$(dirname "$(readlink -f "$0")")
+
 printf 'Removing comment \n'
-sed -i -e "/\s*#.*/s/\s*#.*//" -e "/^\s*$/d" gpgkeys
+sed -i -e "/\s*#.*/s/\s*#.*//" -e "/^\s*$/d" "$rundir/gpgkeys"
 
 printf 'Retriving gpgkeys \n'
-while read line; do
+while read -r line; do
         echo "### adding $line"
 		gpg --recv-keys --keyserver 'keyserver.ubuntu.com' "$line"
 		echo "###"
-done < gpgkeys
+done < "$rundir/gpgkeys"
--- check-dep-and-trigger/check_dep.sh
+++ check-dep-and-trigger/check_dep.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
+rundir=$(dirname "$(readlink -f "$0")")
+
 echo "-- Check one time builded packages and them to pkglist"
 
 sudo pacman -Sy
 
 pacman -Sl needrelax
 
-pacman -Sl needrelax |awk '{print $2}' > actualpkglist
+pacman -Sl needrelax |awk '{print $2}' > "$rundir/actualpkglist"
 
 sort pkglist actualpkglist | uniq > totalpkglist
 
 echo "-- Check packages dependencies --"
 
-while read line;
-  do aur depends -n "$line" >> pkg-depend-list;
-done < totalpkglist
+while read -r line;
+  do aur depends -n "$line" >> "$rundir/pkg-depend-list";
+done < "$rundir/totalpkglist"
 
 echo "-- Total pkg num --"
 
-wc -l < pkg-depend-list
+wc -l < "$rundir/pkg-depend-list"
 
 echo "-- Total pkg list --"
 
-cat pkg-depend-list
+cat "$rundir/pkg-depend-list"
